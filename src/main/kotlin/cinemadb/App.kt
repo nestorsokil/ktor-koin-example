e0e41044e33864da241a636c5f4eaecb2bf8e684
robertt1234 <17678057+robertt1234@users.noreply.github.com>
2019-01-16 21:38:14 +0200

    Added DefaultHeaders feature

--- src/main/kotlin/cinemadb/App.kt
+++ src/main/kotlin/cinemadb/App.kt
@@ -3,9 +3,11 @@ package cinemadb
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
 
 fun Application.main() {
 
+    install(DefaultHeaders)
     install(CallLogging)
 
 }
