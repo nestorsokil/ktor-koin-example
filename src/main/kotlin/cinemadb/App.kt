e5400b2db2a37f0b82adab64593c40fac65d7946
robertt1234 <17678057+robertt1234@users.noreply.github.com>
2019-01-16 21:34:23 +0200

    Added CallLogging feature

--- src/main/kotlin/cinemadb/App.kt
+++ src/main/kotlin/cinemadb/App.kt
@@ -1,7 +1,11 @@
 package cinemadb
 
 import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.features.CallLogging
 
 fun Application.main() {
+
+    install(CallLogging)
 
 }
