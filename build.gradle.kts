6456936fef56d2017d7bcf83691a530eab825f1a
robertt1234 <17678057+robertt1234@users.noreply.github.com>
2019-01-16 21:31:12 +0200

    Added logback logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,14 @@ repositories {
 }
 
 val ktor_version = "1.1.1"
+val logback_version = "1.2.3"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     implementation("io.ktor:ktor-server-netty:$ktor_version")
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
