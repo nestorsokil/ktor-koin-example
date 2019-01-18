a1de3dafaad125f68fa5858e868029df9de40345
robertt1234 <17678057+robertt1234@users.noreply.github.com>
2019-01-18 23:21:24 +0200

    Added persons table

--- src/main/resources/db/migration/V1__create_movies_table.sql
+++ src/main/resources/db/migration/V1__create_movies_table.sql
@@ -1,11 +1,30 @@
 CREATE TABLE movies
 (
-  id           INT          NOT NULL,
+  id           CHAR(36)     NOT NULL,
+  version      INT          NOT NULL DEFAULT 1,
   name         VARCHAR(255) NOT NULL,
   genre        VARCHAR(255) NOT NULL,
   duration     VARCHAR(255) NOT NULL,
   release_date DATE         NOT NULL,
+  created      TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+  updated      TIMESTAMP             DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
   UNIQUE (name, release_date)
 )
   CHARACTER SET = utf8;
+
+CREATE TABLE persons
+(
+  id         CHAR(36)                NOT NULL,
+  version    INT                     NOT NULL DEFAULT 1,
+  first_name VARCHAR(255)            NOT NULL,
+  last_name  VARCHAR(255)            NOT NULL,
+  gender     ENUM ('MALE', 'FEMALE') NOT NULL,
+  age        INT                     NOT NULL,
+  created    TIMESTAMP                        DEFAULT CURRENT_TIMESTAMP,
+  updated    TIMESTAMP                        DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+)
+  CHARACTER SET = utf8;
+
+
